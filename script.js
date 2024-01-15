fe5bc9b26ed47798511234159e4b66c6b0659d25
thakurakhilesh1998 <thakurakhilesh20@gmail.com>
2024-01-15 22:33:02 +0530

    Text utility

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    function updateCharacterCount() {
+        var text = $('#textInput').val();
+        var charCount = text.length;
+        $('#result').text('Character Count: ' + charCount);
+    }
+
+    // Count characters on page load
+    updateCharacterCount();
+
+    // Count characters dynamically as the user types
+    $('#textInput').on('input', function() {
+        updateCharacterCount();
+    });
+
+    // Count words on button click
     $('#countWordsBtn').on('click', function() {
         var text = $('#textInput').val();
         var wordCount = text.split(/\s+/).filter(function(word) {
@@ -8,10 +23,45 @@ $(document).ready(function() {
         $('#result').text('Word Count: ' + wordCount);
     });
 
+    // Count characters on button click
     $('#countCharactersBtn').on('click', function() {
-        var text = $('#textInput').val();
-        var charCount = text.length;
+        updateCharacterCount();
+    });
 
-        $('#result').text('Character Count: ' + charCount);
+    // Text Reversal
+    $('#reverseTextBtn').on('click', function() {
+        var text = $('#textInput').val();
+        var reversedText = text.split('').reverse().join('');
+        $('#result').text('Reversed Text: ' + reversedText);
+    });
+
+    // Text Uppercase
+    $('#uppercaseBtn').on('click', function() {
+        var text = $('#textInput').val();
+        $('#result').text('Uppercase Text: ' + text.toUpperCase());
+    });
+
+    // Text Lowercase
+    $('#lowercaseBtn').on('click', function() {
+        var text = $('#textInput').val();
+        $('#result').text('Lowercase Text: ' + text.toLowerCase());
+    });
+
+    // Find and Replace
+    $('#findReplaceBtn').on('click', function() {
+        var findText = $('#findText').val();
+        var replaceText = $('#replaceText').val();
+        var text = $('#textInput').val();
+        var replacedText = text.replace(new RegExp(findText, 'g'), replaceText);
+        $('#result').text('Replaced Text: ' + replacedText);
+    });
+
+    // Add other features here...
+
+    // Clear Text
+    $('#clearTextBtn').on('click', function() {
+        $('#textInput').val('');
+        $('#result').empty();
+        updateCharacterCount();
     });
 });
